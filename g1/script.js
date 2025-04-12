46f5caecd74f76716a0c0be0f77e9e5e4d78c3ad
palin9201 <palin9201@gmail.com>
2025-04-12 16:16:34 +0800

    Create script.js

---
+++ g1/script.js
@@ -0,0 +1,57 @@
+// Data retrieved from https://netmarketshare.com/
+// Make monochrome colors
+const colors = Highcharts.getOptions().colors.map((c, i) =>
+    // Start out with a darkened base color (negative brighten), and end
+    // up with a much brighter color
+    Highcharts.color(Highcharts.getOptions().colors[0])
+        .brighten((i - 3) / 7)
+        .get()
+);
+
+// Build the chart
+Highcharts.chart('container', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: '台中市主要天氣型態'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            colors,
+            borderRadius: 5,
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                distance: -50,
+                filter: {
+                    property: 'percentage',
+                    operator: '>',
+                    value: 4
+                }
+            }
+        }
+    },
+    series: [{
+        name: '台中市主要天氣型態',
+        data: [
+            { name: '晴朗或多雲', y: 58.0},
+            { name: '降雨', y: 20},
+            { name: '陰天', y: 22 },
+           
+        ]
+    }]
+});
